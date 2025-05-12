0a58ca672ddc44dbbd68102d85f73e29d4efa1fd
k1ngdom-code <okohm256@gmail.com>
2025-05-12 14:47:54 -0700

    Work

---
+++ dropdown.js
@@ -0,0 +1,7 @@
+const dropdownmenu=document.getElementById("drop");
+
+function OpenProfile(){if(dropdownmenu.style.display ==="none"){
+  dropdownmenu.style.display="block"
+}else{
+  dropdownmenu.style.display="none"
+} }
---
+++ sidebar.js
@@ -0,0 +1,5 @@
+function showSideBar(){
+  document.getElementById("sidebr").style.transform="translateX(-300px)"
+} function hideSideBar(){
+  document.getElementById("sidebr").style.transform="translateX(300px)"
+}
